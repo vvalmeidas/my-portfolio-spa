05b3c8135e9530372213ab5773db78821ca76690
Valmir Almeida <vvalmeida96@gmail.com>
2020-09-02 21:08:10 -0300

    :gem: Improving the build

---
+++ build/precache-manifest.93eadafbddd980473aa9bfa4cd0d83e3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55d82f4dd335e15c232cd02831daf499",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48bbde15324c19fef538",
+    "url": "/static/css/main.2a113ebc.chunk.css"
+  },
+  {
+    "revision": "cc69ef3272f6a411bafe",
+    "url": "/static/js/2.d15642e3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d15642e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48bbde15324c19fef538",
+    "url": "/static/js/main.908f5736.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5e1700cd3a1359d2689dd69f61840883",
+    "url": "/static/media/icon.5e1700cd.jpg"
+  },
+  {
+    "revision": "2b9532d476c551a5222532b32be34816",
+    "url": "/static/media/tecnoexpress.2b9532d4.png"
+  },
+  {
+    "revision": "3c1c3de83d91b6986645b81757336935",
+    "url": "/static/media/twitter-painel.3c1c3de8.png"
+  },
+  {
+    "revision": "a776374528c65fb5224b07444dbc6677",
+    "url": "/static/media/video-downloader.a7763745.jpg"
+  }
+]);
